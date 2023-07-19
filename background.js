8b539ff34d26cd9c920404395970f5056d456c20
bendellwerry <139977961+bendellwerry@users.noreply.github.com>
2023-07-19 12:38:16 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-// background.js
-//chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info) => console.log(info)); // Not necessary once tested
-
-
 var clown = "https://media.gab.com/system/reaction_types/images/000/000/005/original/f3671ee770e64718.png"; 
 var honk = "https://media.gab.com/system/custom_emojis/images/000/008/162/original/cd90e365395d1541.png";
 var rules = [{
